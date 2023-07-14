fc86bd28c59b463e0212661b533f60c9cf0bc0c2
Matheus Pereira <contato@matheusp.com.br>
2023-07-14 19:53:16 -0300

    fix: fixing tests with github actions

--- config/database.php
+++ config/database.php
@@ -60,7 +60,7 @@ return [
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'userstransactions') . '_test',
+            'database' => env('DB_DATABASE_TEST', 'userstransactions_test'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
